06792a6114ff3c378ae8660c4b243ada3aa1c5ab
yassir mokhtari <mokhtariyasser3@gmail.com>
2024-04-26 16:19:09 +0100

    26/04/2024 16:19 role service

--- microservice-d-authentification-front-end/src/app/app.component.ts
+++ microservice-d-authentification-front-end/src/app/app.component.ts
@@ -1,12 +1,7 @@
 import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CalendarModule} from "primeng/calendar";
-import {CheckboxModule} from "primeng/checkbox";
-import {FormsModule} from "@angular/forms";
-import {DropdownModule} from "primeng/dropdown";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {UserService} from "./services/user.service";
-import {User} from "./models/user";
 
 @Component({
   selector: 'app-root',
@@ -14,25 +9,8 @@ import {User} from "./models/user";
   imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers:[UserService]
+  providers:[]
 })
-export class AppComponent implements OnInit{
-
-  constructor(
-    private userService: UserService
-  ) {}
-
-
-  ngOnInit() {
-    this.getAllUsers();
-  }
-
-  getAllUsers(){
-    this.userService.findAll().subscribe(
-      (users: User[]) => {
-        console.log(users);
-      }
-    )
-  }
+export class AppComponent{
 
 }
--- microservice-d-authentification-front-end/src/app/services/role.service.ts
+++ microservice-d-authentification-front-end/src/app/services/role.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../models/user";
+import {Role} from "../models/role";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleService {
 
-  constructor() { }
+  private API = "http://localhost:8080/api/Role/"
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Array<Role>> {
+    return this.http.get<Array<Role>>( this.API +'roles/');
+  }
+
+  save(role: Role): Observable<Role> {
+    return this.http.post<Role>(this.API + 'save-role/', role);
+  }
+
+  findById(id: number): Observable<User> {
+    return this.http.get<User>(this.API + 'by-id/' + id);
+  }
+
+  delete(id: number) {
+      return  this.http.delete<void>(this.API + 'delete-role/' + id);
+    }
 }
